7a67507c2a061790ba27ca02fd725ca8c6836172
Pablo Gutiérrez <gutiii1997@gmail.com>
2021-06-15 20:27:45 +0200

    feat: add vue

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+import Vue from 'vue'
+import App from './src/components/App.vue'
 import * as tf from '@tensorflow/tfjs'
-console.log(tf.version)
+
+document.addEventListener('DOMContentLoaded', function(_) {
+  new Vue({
+    render: createElement => createElement(App),
+  }).$mount('#app')
+})
---
+++ src/components/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Prueba</h1>
+</template>
+
+<style lang="scss" scoped>
+html,
+body {
+  color: green;
+}
+</style>
