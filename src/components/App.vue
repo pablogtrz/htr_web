8b57ab5bc13b9361740695a0b7beeac6ad3f2011
Pablo Gutiérrez <gutiii1997@gmail.com>
2021-06-15 23:35:08 +0200

    feat: tensorflow service with Model class

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,6 +2,21 @@
   <h1>Prueba</h1>
 </template>
 
+<script lang="ts">
+import Vue from 'vue'
+import { Model } from '../services/tensorflow'
+
+export default Vue.extend({
+  async created() {
+    const model = await Model.createFrom(
+      'https://raw.githubusercontent.com/tensorflow/tfjs-examples/master/abalone-node/trainedModel/model.json'
+    )
+    model.summary()
+    console.log(model.predict([[1]]))
+  },
+})
+</script>
+
 <style lang="scss" scoped>
 html,
 body {
